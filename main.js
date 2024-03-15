7d1dc048ce77768b4092e72157cd9abf2fb18574
woiciechowshi <151676104+woiciechowshi@users.noreply.github.com>
2024-03-14 21:13:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+
+    fabric.Image.fromURL('AniversarioImagem.png', function(Img) {
+        blockImageObject = Img;
+        blockImageObject.scaleToWidth (700);
+        blockImageObject.scaleToHeight(510);
+        blockImageObject.set({
+        top: 0,
+        left:0
+        });
+        canvas.add(blockImageObject);
+        });
+
+
+}
+
+function playsound(){
+x.play();
+}
